635c71bddf92de60f076547bf2c4834927bfb340
HKY45 <mufaathadirosid@gmail.com>
2024-02-01 15:29:27 +0700

    7. Post Model

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -2,10 +2,15 @@
 
 @section('container')
   <article>
-    <h2>{{ $post["title"] }}</h2>
-    <h5>{{ $post["author"] }}</h5>
-    <p>{{ $post["body"] }}</p>
+    <h2>{{ $post->title }}</h2>
+    {!! $post->body !!}
   </article>
 
   <a href="/posts">Back to Posts</a>
 @endsection
+{{-- Post::create([
+  'title' => "Judul Ketiga",
+  'slug' => "judul-ketiga",
+  'excerpt' => "Lorem ipsum dolor sit amet consectetur adipisicing Ketiga",
+  'body' => "<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste tempore unde praesentium deserunt, veniam adipisci cum vitae, expedita ipsam totam quam. </p><p>Suscipit, quaerat placeat porro quasi quisquam ab hic facilis alias iure, quos nemo eligendi, itaque consectetur aspernatur ad numquam? Accusantium nulla labore exercitationem pariatur consequatur. Ut itaque quae similique, eveniet architecto impedit cumque nesciunt iure. Totam odio tempore porro. Mollitia cupiditate quia, accusantium iure accusamus consequuntur inventore praesentium eos tenetur necessitatibus tempore temporibus culpa qui, sint dolore explicabo autem enim dolores labore dolor sit aspernatur? </p><p>Maxime repudiandae sapiente accusamus vero libero. Aspernatur blanditiis eligendi nobis nemo necessitatibus odit ad, dignissimos unde illo explicabo odio quod ab harum consectetur dolores voluptatibus eaque debitis aperiam eius delectus ex ut! Cupiditate eius aliquam laudantium excepturi sunt aperiam ea, illum rerum doloremque eos consequatur tempore autem unde expedita ad. Fugit quibusdam officia natus. Saepe sequi perspiciatis facilis voluptatem non ipsa quod? Sequi, officiis?</p>"
+]) --}}
