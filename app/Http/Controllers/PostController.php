95cbbae4ef33e10baa2fb7460c33d5b1977525de
HKY45 <mufaathadirosid@gmail.com>
2024-02-05 09:05:41 +0700

    13. Searching & Pagination

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,19 +2,32 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-use \App\Models\Post;
 use Illuminate\Support\Facades\Http;
+use Illuminate\Http\Request;
+use \App\Models\Category;
+use \App\Models\Post;
+use \App\Models\User;
 
 class PostController extends Controller
 {
     public function index()
     {
+        $title = '';
+        if (request('category')) {
+            $category = Category::firstWhere('slug', request('category'));
+            $title = ' in ' . $category->name;
+        }
+        if (request('author')) {
+            $author = User::firstWhere('username', request('author'));
+            $title = ' by ' . $author->name;
+        }
+
+        $posts = Post::latest()->filter(request(['search', 'category', 'author']))->paginate(4)->withQueryString();
+
         return view('posts', [
-            "title" => "All Posts",
+            "title" => "All Posts" . $title,
             "active" => "posts",
-            // "posts" => Post::all()
-            "posts" => Post::latest()->get()
+            "posts" => $posts
         ]);
     }
 
