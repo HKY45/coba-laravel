7901dd3e6a6dd7beaa755cc88dc788474e02d978
HKY45 <mufaathadirosid@gmail.com>
2024-02-21 12:24:00 +0700

    23. Authorization

--- resources/views/login/index.blade.php
+++ resources/views/login/index.blade.php
@@ -3,22 +3,22 @@
 @section('container')
 <div class="row justify-content-center mt-6" style="margin-top: 20px">
     <div class="col-lg-4">
-
-        @if (session()->has('success'))  
-        <div class="alert alert-success alert-dismissible fade show" role="alert">
-            {{ session('success') }}
-            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-        </div>
-        @endif
-        @if (session()->has('loginError'))  
-        <div class="alert alert-danger alert-dismissible fade show" role="alert">
-            {{ session('loginError') }}
-            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-        </div>
-        @endif
-
         <main class="form-signin">
             <h1 class="h3 mb-3 fw-normal text-center">Please login</h1>
+            @if (session()->has('success'))  
+                <div class="alert alert-success alert-dismissible fade show" role="alert">
+                    {{ session('success') }}
+                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                </div>
+            @endif
+            @if (session()->has('loginError'))  
+                <div class="alert alert-danger alert-dismissible fade show" role="alert" role="alert">
+                    {{ session('loginError') }}
+                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+            @endif
             <img src="https://source.unsplash.com/1200x600?singapore" class="card-img-top rounded mb-3" alt="japan">
             <form action="/login" method="POST">
                 @csrf
